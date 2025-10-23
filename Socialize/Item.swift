c6d1945ab98c98b3907c6ac3443af602e7e75fc5
micah-earl <ear24005@byui.edu>
2025-10-23 13:36:49 -0600

    Initial Commit

---
+++ Socialize/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Socialize
+//
+//  Created by Micah Earl on 10/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SocializeTests/SocializeTests.swift
@@ -0,0 +1,16 @@
+//
+//  SocializeTests.swift
+//  SocializeTests
+//
+//  Created by Micah Earl on 10/23/25.
+//
+
+import Testing
+
+struct SocializeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
